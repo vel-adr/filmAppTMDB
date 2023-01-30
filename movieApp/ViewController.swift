6fae853bcc86a574770fddab3fc2e44faedf571f
vel-adr <anselmus.pavel@gmail.com>
2023-01-30 20:32:29 +0700

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Anselmus Pavel Adriska on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
